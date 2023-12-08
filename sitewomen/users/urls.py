9becaef64ac2236ea7c444873200b7e79080c3ce
Закиев Евгений Андреевич <e.zakiev@CZNNVR.local>
2023-12-08 15:30:33 +0300

    add: password reset

--- sitewomen/users/urls.py
+++ sitewomen/users/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth.views import LogoutView
-from django.contrib.auth.views import PasswordChangeDoneView
+from django.contrib.auth.views import (
+    PasswordChangeDoneView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 
 from . import views
 
@@ -21,5 +27,47 @@ urlpatterns = [
     path("profile/", views.ProfileUser.as_view(), name="profile"),
     path(
         "password-change/", views.UserPasswordChange.as_view(), name="password_change"
+    ),
+
+    # path(
+    #     "password-reset/",
+    #     PasswordResetView.as_view(template_name="users/password_reset_form.html"),
+    #     name="password_reset",
+    # ),
+    path(
+        "password-reset/",
+        PasswordResetView.as_view(
+            template_name="users/password_reset_form.html",
+            email_template_name="users/password_reset_email.html",
+            success_url=reverse_lazy("users:password_reset_done"),
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
+        name="password_reset_done",
+    ),
+    # path(
+    #     "password-reset/<uidb64>/<token>/",
+    #     PasswordResetConfirmView.as_view(
+    #         template_name="users/password_reset_confirm.html"
+    #     ),
+    #     name="password_reset_confirm",
+    # ),
+    path(
+        "password-reset/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(
+            template_name="users/password_reset_confirm.html",
+            success_url=reverse_lazy("users:password_reset_complete"),
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset/complete/",
+        PasswordResetCompleteView.as_view(
+            template_name="users/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
     ),
 ]
